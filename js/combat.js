6e576e415e44e1f0df1b8d3ad4d0f7708e677752
elona <simon.elona@hotmail.com>
2018-09-28 10:10:15 +0200

    retour en arriere

--- js/combat.js
+++ js/combat.js
@@ -4,26 +4,26 @@ function fight() {
     alert("que le combat commence");
     alert(player.name + " commence");
     if (player.name = "joueurRouge") {
-        players[1].receivesDamage(players[0]);
+        players[1].receivesDamage(players[0], 'attack');
     } else {
-        players[0].receivesDamage(players[1]);
+        players[0].receivesDamage(players[1], 'attack');
     }
-    while (game.players[0].health > 0 && game.players[1].health > 0) {
+    while (players[0].health > 0 && players[1].health > 0) {
         var player1Choice = prompt(" joueur 1 :Entrer A pour attaquer et D pour vous défendre");
         var player2Choice = prompt("joueur 2 : Entrer A pour attaquer et D pour vous défendre");
 
         if (player1Choice === "a" && player2Choice === "a") {
-            players[0].receivesDamage(players[1]);
-            players[1].receivesDamage(players[0]);
+            players[0].receivesDamage(players[1], 'attack');
+            players[1].receivesDamage(players[0], 'attack');
         } else if (player1Choice === "d" && player2Choice === "a") {
-            players[0].receivesDamage(players[1]) / 2;
+            players[0].receivesDamage(players[1], 'defend');
         } else if (player1Choice === "a" && player2Choice === "d") {
-            players[1].receivesDamage(players[0]) / 2;
+            players[1].receivesDamage(players[0], 'defend');
         } else if (player1Choice === "d" && player2Choice === "d") {
             alert("les points de vie des joueurs n'ont pas changés")
         }
     }
-    if (game.players[0].health === 0) {
+    if (players[0].health === 0) {
         alert("joueur Rouge à gagné")
     } else {
         alert("joueur bleu à gagné")
--- js/initialisation.js
+++ js/initialisation.js
@@ -1,5 +1,5 @@
 //création des armes;
-
+var weapons = [];
 //modèle de l'objet armes
 var weaponEx = {
     init: function(name, damage, image) {
@@ -9,6 +9,7 @@ var weaponEx = {
     }
 };
 
+var players = [];
 //modèle de l'objet joueurs
 var newPlayer = {
     init: function(name, weapon, image, left, right, up, down) {
@@ -26,24 +27,48 @@ var newPlayer = {
         return parseFloat($('#' + this.name).parent().attr("id"));
     },
     //permet de stocker les dégats fait par un joueur
-    getDamage: function() {
-        return this.weapon.damage;
+    getDamage: function(ratio) {
+        return this.weapon.damage * ratio;
     },
     //fonction permettant d'enlever les points de vie au joueur
-    receivesDamage: function(opponent) {
-        this.health -= opponent.getDamage();
-        alert("PV de " + this.name + ' : ' + players[0].health + ' pv ');
+    receivesDamage: function(opponent, action) {
+        if (action === "attack") {
+            this.health -= opponent.getDamage(1);
+        } else if (action === "defend") {
+            this.health -= opponent.getDamage(0.5);
+        }
+        $('.sante').text("santé " + parseInt(player.health));
     },
     //fonction contenant les actions d'un mouvement
-    movementActions: function(idOperator) {
-        id = this.positionCaseId() + idOperator;
+    moveToLeft: function() {
+        id = this.positionCaseId() - 1;
         $('#' + this.name).appendTo($("#" + id + ""));
         this.changeWeapon(id);
         this.alternatePlayer();
         board.startFight();
-
-        $('.sante').text("santé " + parseInt(player.health));
     },
+    moveToRight: function() {
+        id = this.positionCaseId() + 1;
+        $('#' + this.name).appendTo($("#" + id + ""));
+        this.changeWeapon(id);
+        this.alternatePlayer();
+        board.startFight();
+    },
+    moveToBottom: function() {
+        id = this.positionCaseId() + 10;
+        $('#' + this.name).appendTo($("#" + id + ""));
+        this.changeWeapon(id);
+        this.alternatePlayer();
+        board.startFight();
+    },
+    moveToTop: function() {
+        id = this.positionCaseId() - 10;
+        $('#' + this.name).appendTo($("#" + id + ""));
+        this.changeWeapon(id);
+        this.alternatePlayer();
+        board.startFight();
+    },
+
     //fonction de changement de joueurs
     changePlayer: function() {
 
@@ -75,148 +100,135 @@ var newPlayer = {
                 };
 
             };
-        }
+        };
+    }
+};
+//création de l'objet plateau;
+var board = {
+    nbCol: 10,
+    nbRow: 10,
+    nbCase: 100,
+    nbObstacle: 20,
+    //initialisation du plateau
+    init: function() {
+        //ligne
+        for (var i = 0; i < this.nbRow; i++) {
+            $('<div class="ligne"></div>').appendTo($("#jeu"));
+
+        };
+        //colonne
+        for (var j = 0; j < this.nbCol; j++) {
+            $(`<div class="case"></div>`).appendTo($(".ligne"));
+        };
+        //donne un id de 1 $ 100 à chaque case pour le repérages des éléments
+        for (var i = 0; i < this.nbCase; i++) {
+            var nombre = parseInt(i + 1);
+            var cases = document.getElementsByClassName('case');
+            cases[i].setAttribute("id", nombre);
+        };
+        //génération aléatoire des obstacles;
+        var placedObstacle = 0;
+        while (placedObstacle < this.nbObstacle) {
+            var randomCase = parseFloat(Math.floor(Math.random() * this.nbCase));
+            var obstacleCase = $('.case')[randomCase];
+            if (obstacleCase.style.backgroundColor !== "grey") {
+                var obstacle = $('.case')[randomCase];
+                obstacle.style.backgroundColor = "grey";
+                placedObstacle++;
+            };
+        };
+        //positionnement des joueurs;
+        placedPlayers = 0;
+        while (placedPlayers < players.length) {
+            var randomCase = parseFloat(Math.floor(Math.random() * this.nbCase));
+            var idPlayerStartingCase = $('.case')[randomCase];
+            if (idPlayerStartingCase.style.backgroundColor !== "grey") {
+                $("<img class='player' id=" + players[placedPlayers].name + " src='" + players[placedPlayers].image + "' alt='" + players[placedPlayers].name + "'/>").appendTo($('.case')[randomCase]);
+                placedPlayers++;
+            };
+        };
+        //positionnement des armes ;
+        placedWeapons = 0;
+        while (placedWeapons < 3) {
+            var randomCase = parseFloat(Math.floor(Math.random() * this.nbCase));
+            var idWeaponCase = $('.case')[randomCase];
+            if (idWeaponCase.style.backgroundColor !== "grey") {
+                $("<img class='weapon' id=" + weapons[placedWeapons].name + " src='" + weapons[placedWeapons].image + "' alt='" + weapons[placedWeapons].name + "'/>").appendTo($('.case')[randomCase]);
+                placedWeapons++
+            };
+        };
     },
+    //vérifie si la case ciblée est un obstacle
+    isTargetedCaseAnObstacle: function(idTargetedCase) {
+        return $("#" + idTargetedCase + "").css("backgroundColor") === "rgb(128, 128, 128)";
+    },
+    //vérifie si la case de droite est accessible
+    isRightAccessible: function() {
+        var idRightCase = player.positionCaseId() + 1;
+        return !(player.positionCaseId() % 10 === 0 || this.isTargetedCaseAnObstacle(idRightCase));
+    },
+    //vérifie si la case de gauche est accessible
+    isLeftAccessible: function() {
+        var idLeftCase = player.positionCaseId() - 1;
+        return !(player.positionCaseId() % 10 === 1 || this.isTargetedCaseAnObstacle(idLeftCase));
+    },
+    //vérifie si la case du dessus est accessible
+    isTopAccessible: function() {
+        var idTopCase = player.positionCaseId() - 10;
+        return !(player.positionCaseId() <= 10 || this.isTargetedCaseAnObstacle(idTopCase));
+    },
+    //vérifie si la case du dessous est accessible
+    isBottomAccessible: function() {
+        var idBottomCase = player.positionCaseId() + 10;
+        return !(player.positionCaseId() > 90 || this.isTargetedCaseAnObstacle(idBottomCase));
+    },
+    //vérifie si les joueurs sont côte côte est lance le combat si oui
+    startFight: function() {
 
-    //création de l'objet plateau;
-    board: {
-        nbCol: 10,
-        nbRow: 10,
-        nbCase: 100,
-        nbObstacle: 20,
-        //initialisation du plateau
-        init: function() {
-            //ligne
-            for (var i = 0; i < this.nbRow; i++) {
-                $('<div class="ligne"></div>').appendTo($("#jeu"));
-
-            };
-            //colonne
-            for (var j = 0; j < this.nbCol; j++) {
-                $(`<div class="case"></div>`).appendTo($(".ligne"));
-            };
-            //donne un id de 1 $ 100 à chaque case pour le repérages des éléments
-            for (var i = 0; i < this.nbCase; i++) {
-                var nombre = parseInt(i + 1);
-                var cases = document.getElementsByClassName('case');
-                cases[i].setAttribute("id", nombre);
-            };
-            //génération aléatoire des obstacles;
-            var placedObstacle = 0;
-            while (placedObstacle < this.nbObstacle) {
-                var randomCase = parseFloat(Math.floor(Math.random() * this.nbCase));
-                var obstacleCase = $('.case')[randomCase];
-                if (obstacleCase.style.backgroundColor !== "grey") {
-                    var obstacle = $('.case')[randomCase];
-                    obstacle.style.backgroundColor = "grey";
-                    placedObstacle++;
-                };
-            };
-            //positionnement des joueurs;
-            placedPlayers = 0;
-            while (placedPlayers < game.players.length) {
-                var randomCase = parseFloat(Math.floor(Math.random() * this.nbCase));
-                var idPlayerStartingCase = $('.case')[randomCase];
-                if (idPlayerStartingCase.style.backgroundColor !== "grey") {
-                    $("<img class='player' id=" + game.players[placedPlayers].name + " src='" + game.players[placedPlayers].image + "' alt='" + game.players[placedPlayers].name + "'/>").appendTo($('.case')[randomCase]);
-                    placedPlayers++;
-                };
-            };
-            //positionnement des armes ;
-            placedWeapons = 0;
-            while (placedWeapons < 3) {
-                var randomCase = parseFloat(Math.floor(Math.random() * this.nbCase));
-                var idWeaponCase = $('.case')[randomCase];
-                if (idWeaponCase.style.backgroundColor !== "grey") {
-                    $("<img class='weapon' id=" + game.weapons[placedWeapons].name + " src='" + game.weapons[placedWeapons].image + "' alt='" + game.weapons[placedWeapons].name + "'/>").appendTo($('.case')[randomCase]);
-                    placedWeapons++
-                };
-            };
-        },
-        //vérifie si la case ciblée est un obstacle
-        isTargetedCaseAnObstacle: function(idTargetedCase) {
-            return $("#" + idTargetedCase + "").css("backgroundColor") === "rgb(128, 128, 128)";
-        },
-        //vérifie si la case de droite est accessible
-        isRightAccessible: function() {
-            var idRightCase = player.positionCaseId() + 1;
-            return !(player.positionCaseId() % 10 === 0 || this.isTargetedCaseAnObstacle(idRightCase));
-        },
-        //vérifie si la case de gauche est accessible
-        isLeftAccessible: function() {
-            var idLeftCase = player.positionCaseId() - 1;
-            return !(player.positionCaseId() % 10 === 1 || this.isTargetedCaseAnObstacle(idLeftCase));
-        },
-        //vérifie si la case du dessus est accessible
-        isTopAccessible: function() {
-            var idTopCase = player.positionCaseId() - 10;
-            return !(player.positionCaseId() <= 10 || this.isTargetedCaseAnObstacle(idTopCase));
-        },
-        //vérifie si la case du dessous est accessible
-        isBottomAccessible: function() {
-            var idBottomCase = player.positionCaseId() + 10;
-            return !(player.positionCaseId() > 90 || this.isTargetedCaseAnObstacle(idBottomCase));
-        },
-        //vérifie si les joueurs sont côte côte est lance le combat si oui
-        startFight: function() {
-
-            nearbyCase = [player.positionCaseId() + 1, player.positionCaseId() - 1, player.positionCaseId() + 10, player.positionCaseId() - 10];
-            for (var i = 0; i < nearbyCase.length; i++) {
-                if ($("#" + parseInt(nearbyCase[i]) + " :first-child").attr("class") === "player") {
-                    fight();
-                }
+        nearbyCase = [player.positionCaseId() + 1, player.positionCaseId() - 1, player.positionCaseId() + 10, player.positionCaseId() - 10];
+        for (var i = 0; i < nearbyCase.length; i++) {
+            if ($("#" + parseInt(nearbyCase[i]) + " :first-child").attr("class") === "player") {
+                fight();
             }
         }
-    },
+    }
+}
 
-
+var game = {
     init: function() {
-        var weapons = [];
-        var players = [];
+
+
+        //initialisation des différentes armes
+        var scepter = Object.create(weaponEx);
+        scepter.init("Sceptre", 20, "image/sceptre.jpg");
+        var sling = Object.create(weaponEx);
+        sling.init("Fronde", 15, "image/fronde.jpg");
+        var wood = Object.create(weaponEx);
+        wood.init("Bois", 5, "image/bois.jpg");
+        var rapier = Object.create(weaponEx);
+        rapier.init("Rapière", 10, "image/rapiere.jpg");
+        var sword = Object.create(weaponEx);
+        sword.init("epee", 10, "image/epee.jpg");
+        weapons.push(scepter, sling, wood, rapier, sword);
         //initialisation des différents personnages
         var playerOne = Object.create(newPlayer);
         playerOne.init("joueurRouge", weapons[3], "image/joueur1.jpg", 81, 68, 90, 83);
         var playerTwo = Object.create(newPlayer);
         playerTwo.init("joueurBleu", weapons[4], "image/joueur2.jpg", 37, 39, 38, 40);
         players.push(playerOne, playerTwo);
-        //initialisation des différentes armes
-        var scepter = Object.create(this.weaponEx);
-        scepter.init("Sceptre", 20, "image/sceptre.jpg");
-        var sling = Object.create(this.weaponEx);
-        sling.init("Fronde", 15, "image/fronde.jpg");
-        var wood = Object.create(this.weaponEx);
-        wood.init("Bois", 5, "image/bois.jpg");
-        var rapier = Object.create(this.weaponEx);
-        rapier.init("Rapière", 10, "image/rapiere.jpg");
-        var sword = Object.create(this.weaponEx);
-        sword.init("epee", 10, "image/epee.jpg"); <<
-        <<
-        << < HEAD
-        this.weapons.push(scepter, sling, wood, rapier, sword);
-        //initialisation des différents personnages
-        var playerOne = Object.create(this.newPlayer);
-        playerOne.init("joueurRouge", this.weapons[3], "image/joueur1.jpg", 81, 68, 90, 83);
-        var playerTwo = Object.create(this.newPlayer);
-        playerTwo.init("joueurBleu", this.weapons[4], "image/joueur2.jpg", 37, 39, 38, 40);
-        this.players.push(playerOne, playerTwo); ===
-        ===
-        =
-        weapons.push(scepter, sling, wood, rapier, sword); >>>
-        >>>
-        > parent of e01ef17...preparation mentorat
-            //affichage de la grille
-        this.board.init()
-
-    },
+        //affichage de la grille
+        board.init()
+    }
 };
+
 //initialisation du jeu
 game.init();
 var nbTurn = 0;
 var player = players[1];
 //affichage données des joueurs:;
-$('.droitej1').append(String.fromCharCode(game.players[0].right));
-$('.gauchej1').append(String.fromCharCode(game.players[0].left));
-$('.hautj1').append(String.fromCharCode(game.players[0].up));
-$('.basj1').append(String.fromCharCode(game.players[0].down));
-$('.santej1').text("santé " + parseInt(game.players[0].health));
-$('.santej2').text("santé " + parseInt(game.players[0].health));
+$('.droitej1').append(String.fromCharCode(players[0].right));
+$('.gauchej1').append(String.fromCharCode(players[0].left));
+$('.hautj1').append(String.fromCharCode(players[0].up));
+$('.basj1').append(String.fromCharCode(players[0].down));
+$('.santej1').text("santé " + parseInt(players[0].health));
+$('.santej2').text("santé " + parseInt(players[0].health));
